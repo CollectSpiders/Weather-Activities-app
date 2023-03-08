0282091d0c2e1f90ea89bafd2323773e9d124434
CollectSpiders <98413464+CollectSpiders@users.noreply.github.com>
2023-03-07 19:41:25 -0800

    added the google maps api to embed an interactive map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 let displayWeather = document.getElementById('city-container');
 
 function weatherApi(longitude, latitude) {
-    var weatherUrlRequest = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid={4a4acc2ad2028b514da851eb7125f0ac}`;
-    // when "Google maps" API is implemented, be sure to add in the ${lat} & ${lon} to the API snippet above.
+    var weatherUrlRequest = `https://api.openweathermap.org/data/2.5/weather?q=` + cityName + `&appid={4a4acc2ad2028b514da851eb7125f0ac}`;
+        //`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid={4a4acc2ad2028b514da851eb7125f0ac}`
 
     fetch(weatherUrlRequest)
         .then(function (response) {
