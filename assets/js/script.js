5145e3ba6968f59a3cbaefae4a5bab5595859b19
spamdalfz <coreyvasser@gmail.com>
2023-03-07 19:09:51 -0800

    added event listner to search button to fire off weatherAPi function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,30 @@
 let displayWeather = document.getElementById('city-container');
 
-function weatherApi(latitude, longitude){
-    var weatherUrlRequest = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid={4a4acc2ad2028b514da851eb7125f0ac}`; 
+function weatherApi(longitude, latitude) {
+    var weatherUrlRequest = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid={4a4acc2ad2028b514da851eb7125f0ac}`;
     // when "Google maps" API is implemented, be sure to add in the ${lat} & ${lon} to the API snippet above.
 
-    fetch(weatherUrlRequest){
-        .then(function(response){
+    fetch(weatherUrlRequest)
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data){
+        .then(function (data) {
             console.log(data.list);
             var weatherInfo = data.list;
-            // let name = ;
-            // tie variable to location api when implemented.
 
             weatherInfo.forEach(list => {
-
                 var weatherCard = document.createElement('div');
-                weatherCard.innerHTML = `
-                    <h3 class="text-lg font-bold"></h3>
-                    <p>Current Weather outside is ${weather}</p>
-                    <p class=""> The tempurature outside is currently ${main.temp} and feels like ${main.feels_like}</p>
-                `; 
-                // add an HTML card /w tailwind API CSS info to innerHTML
-                
+                weatherCard.innerHTML = ``;
+                // add an HTML card /w tailwind API CSS info to line 17
+
                 displayWeather.append(weatherCard);
             });
         })
-    }
 }
+
+const searchButton = document.getElementById('search-button');
+searchButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    const cityName = document.querySelector('input[type="text"]').value;
+    weatherApi(cityName);
+});
