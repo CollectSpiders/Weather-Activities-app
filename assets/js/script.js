45ac17ef4ff771ba5af4b9d6a4c80134cc056c2d
Paul-Runge <brfirestarter@gmail.com>
2023-03-09 18:37:56 -0800

    updates i can't thnk of right now

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,33 +8,58 @@ function weatherApi(cityName) {
 
     fetch(weatherUrlRequest)
         .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            var temperature = data.main.temp;
+            response.json()
+            .then(function (data) {
+                console.log(data);
+                var temperature = data.main.temp;
             var description = data.weather[0].description;
+            let lat = data.coord.lat;
+            let lon = data.coord.lon;
+            // console.log(lat);
+            // console.log(lon);
 
             //weatherInfo.forEach(list => 
             var weatherCard = document.createElement('div');
             weatherCard.innerHTML = 
             `<h3 class="text-center text-lg font-bold"> ${description}</h3 ><p>Current temperature is ${temperature}</p>`;
             // add an HTML card /w tailwind API CSS info to line 17
-
+            
             displayWeather.append(weatherCard);
+            })
+            
         });
-}
-
-const searchButton = document.getElementById('search-button');
-const form = document.getElementById('search-form')
-console.log(form);
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const cityName = document.querySelector("#city-input").value;
-    console.log(cityName);
-    if (cityName){
-        weatherApi(cityName);
     }
-});
-
-weatherApi();
+    
+    const searchButton = document.getElementById('search-button');
+    const form = document.getElementById('search-form')
+    console.log(form);
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const cityName = document.querySelector("#city-input").value;
+        console.log(cityName);
+        if (cityName){
+            weatherApi(cityName);
+        }
+    });
+    // Why is this here?--------------
+    // weatherApi();
+    
+    let map;
+function initMap() {
+  
+    // The location of Geeksforgeeks office
+    const gfg_office = {
+        lat: 28.50231,
+        lng: 77.40548
+    };
+    
+    // Create the map, centered at gfg_office
+    const map = new google.maps.Map(
+        document.getElementById("map"), {
+            
+            // Set the zoom of the map
+            zoom: 17.56,
+            center: gfg_office,
+        });
+}    
+window.initMap = initMap;
